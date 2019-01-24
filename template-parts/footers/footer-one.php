87790bcff267a245db4c8f000d8037519faf7eee
syntheticmachine <matt@ugduck.com>
2019-01-24 15:22:34 -0800

    Update screenshot.

--- template-parts/footers/footer-one.php
+++ template-parts/footers/footer-one.php
@@ -33,22 +33,22 @@
 			<?php endif; ?>
 		</div>
 	</div>
-</footer>
-<div class="company__info">
-	<div class="container">
-		<div class="row">
-			<div class="col text-center">
-				<p>© <?php echo date('Y'); ?> <?php echo get_bloginfo('name'); ?> <span>|</span>Created By <a href="https://uglyduckmarketing.com/" target="_blank">UDM</a></p>
+	<div class="company__info">
+		<div class="container">
+			<div class="row">
+				<div class="col text-center">
+					<p>© <?php echo date('Y'); ?> <?php echo get_bloginfo('name'); ?> <span>|</span>Created By <a href="https://uglyduckmarketing.com/" target="_blank">UDM</a></p>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</footer>
 
 <style>
 footer {
 	padding: 3.5rem 0;
+	padding-bottom: 0;
 	border-top: 1px solid rgba(0,0,0,.05);
-	background: #f9f9f9;
 }
 footer img {
   max-height: 75px;
@@ -69,20 +69,21 @@ footer .widget__title {
   font-size: 1rem;
   color: var(--primary);
 }
-.company__info {
+footer .company__info {
 	border-top: 1px solid rgba(0,0,0,.05);
 	padding: .75rem 0;
 	font-size: .75rem;
+	margin-top: 2.5rem;
 }
-.company__info p {
+footer .company__info p {
 	margin-bottom: 0;
 }
-.company__info span {
+footer .company__info span {
 	display: inline-block;
 	margin: 0 .35rem;
 	color: rgba(0,0,0,.165);
 }
-.company__info a {
+footer .company__info a {
 	font-weight: 600;
 	color: var(--primary);
 }
