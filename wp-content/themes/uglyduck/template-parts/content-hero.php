4b57bbd769dad34a7994f564c7dc552e74a75644
syntheticmachine <matt@ugduck.com>
2019-02-11 11:23:02 -0800

    Removed unwanted files

--- wp-content/themes/uglyduck/template-parts/content-hero.php
+++
@@ -1,6 +0,0 @@
-<?php
-global $post;
-if(get_field('hero_override')) { $heroID = get_field('hero_override'); } else { $heroID = get_field('default_hero','options'); }
-$heroTemplate = get_field('hero_layout', $heroID);
-get_template_part( 'template-parts/heroes/hero', $heroTemplate );
-?>
--- wp-content/themes/uglyduck/template-parts/content-page.php
+++
@@ -1,7 +0,0 @@
-<main>
-	<section class="container">
-		<div class="row">
-			<?php if (have_posts()) : while (have_posts()) : the_post(); the_content(); endwhile; endif; ?>
-		</div>
-	</section>
-</main>
