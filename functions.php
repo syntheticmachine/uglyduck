78b3a459916b2297fec4be9fadf92c5c49ac7029
syntheticmachine <matt@ugduck.com>
2019-02-13 15:38:58 -0800

    Removed ACF

--- functions.php
+++ functions.php
@@ -68,6 +68,7 @@ add_filter('upload_mimes', 'cc_mime_types');
 // OPTIONS PAGE
 //======================================================================
 
+/*
 // 1. customize ACF path
 add_filter('acf/settings/path', 'my_acf_settings_path');
 
@@ -93,10 +94,13 @@ function my_acf_settings_dir( $dir ) {
 }
 
 // 3. Hide ACF field group menu item
-// add_filter('acf/settings/show_admin', '__return_false');
+add_filter('acf/settings/show_admin', '__return_false');
+
+*/
 
 // 4. Include ACF
 include_once( get_stylesheet_directory() . '/inc/advanced-custom-fields-pro/pro/acf-pro.php' );
+
 
 //======================================================================
 // OPTIONS PAGE
