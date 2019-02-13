9cdfc140ca4f459b3ac0c31b7c2fa800da34e5fe
syntheticmachine <matt@ugduck.com>
2019-02-13 15:43:21 -0800

    Fixed Field Groups

--- functions.php
+++ functions.php
@@ -68,44 +68,6 @@ add_filter('upload_mimes', 'cc_mime_types');
 // OPTIONS PAGE
 //======================================================================
 
-/*
-// 1. customize ACF path
-add_filter('acf/settings/path', 'my_acf_settings_path');
-
-function my_acf_settings_path( $path ) {
-    // update path
-    $path = get_stylesheet_directory() . '/inc/';
-    // return
-    return $path;
-}
-
-
-// 2. customize ACF dir
-add_filter('acf/settings/dir', 'my_acf_settings_dir');
-
-function my_acf_settings_dir( $dir ) {
-
-    // update path
-    $dir = get_stylesheet_directory_uri() . '/inc/';
-
-    // return
-    return $dir;
-
-}
-
-// 3. Hide ACF field group menu item
-add_filter('acf/settings/show_admin', '__return_false');
-
-*/
-
-// 4. Include ACF
-include_once( get_stylesheet_directory() . '/inc/advanced-custom-fields-pro/pro/acf-pro.php' );
-
-
-//======================================================================
-// OPTIONS PAGE
-//======================================================================
-
 // Create UDM Options Page
 if( function_exists('acf_add_options_page') ) {
 	acf_add_options_page(array(
