d5d73dd12812fa3edc5c4fc7ee9f9c1f339394b5
syntheticmachine <matt@ugduck.com>
2019-02-26 16:24:43 -0800

    Page Builder Functionality

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,42 +1,61 @@
 <main>
+
 <!-- Default Content -->
-<?php if (have_posts()) : while (have_posts()) : the_post();?>
-	<?php if ( !empty( get_the_content() ) ) : ?>
-	<div class="container" style="padding-top: 6.5rem; padding-bottom: 6.5rem;">
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); if ( !empty( get_the_content() ) ) : ?>
+	<div class="container">
 		<div class="row">
 			<?php the_content(); ?>
 		</div>
 	</div>
-<?php endif; ?>
-<?php endwhile; endif; ?>
+<?php endif; endwhile; endif; ?>
+
 <!-- End Default Content -->
 
 <!-- Elements -->
 
-<?php if( have_rows('elements') ): while ( have_rows('elements') ) : the_row(); ?>
-	<?php
-	$backgroundType = get_sub_field('background_type');
-	$backgroundColor = get_sub_field('background_color');
-	$backgroundImage = get_sub_field('background_image');
-	$backgroundPosition = get_sub_field('background_position');
-	if($backgroundType == 'color') {
-		$background = 'background-color: ' . $backgroundColor;
-	} elseif($backgroundType == 'image') {
-		$background = 'background-image: url(' . $backgroundImage . '); background-attachment: ' . $backgroundPosition;
-	}
-	?>
+<?php if( have_rows('elements') ): while ( have_rows('elements') ) : the_row();
+$backgroundType = get_sub_field('background_type');
+$backgroundColor = get_sub_field('background_color');
+$backgroundImage = get_sub_field('background_image');
+$backgroundPosition = get_sub_field('background_position');
+if($backgroundType == 'color') {
+	$background = 'background-color: ' . $backgroundColor;
+} elseif($backgroundType == 'image') {
+	$background = 'background-image: url(' . $backgroundImage . '); background-attachment: ' . $backgroundPosition;
+}
+?>
 
 
 	<section style="<?php echo $background; ?>;">
 
-		<?php if( have_rows('select_element') ) : while ( have_rows('select_element') ) : the_row(); ?>
+		<!-- Fullwidth Content -->
 
-		<?php if( get_row_layout() == 'vendor_logos' ) : ?>
+		<?php if( have_rows('fullwidth_content') ): while( have_rows('fullwidth_content') ): the_row(); ?>
+
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="w-100">
+						<div class="w-<?php the_sub_field('content_width'); ?> <?php the_sub_field('content_position'); ?>">
+							<span class="content__block-eyebrow"><?php the_sub_field('eyebrow'); ?></span>
+							<h1><?php the_sub_field('title'); ?></h1>
+							<?php echo wpautop(get_sub_field('content')); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		<?php endwhile; endif; ?>
+
+		<!-- End Fullwidth Content -->
+
+		<!-- Vendor Gallery -->
+
+		<?php if( have_rows('vendor_gallery') ): while( have_rows('vendor_gallery') ): the_row(); ?>
 
 			<div class="container">
 				<div class="row">
-					<?php
-					$vendorLogos = get_sub_field('vendor_logos');
+					<?php $vendorLogos = get_sub_field('vendor_logos');
 					if( $vendorLogos ): ?>
 					<?php foreach( $vendorLogos as $vendorLogo ): ?>
 						<div class="col-lg-3 col-md-4 col-6">
@@ -49,29 +68,32 @@
 				</div>
 			</div>
 
-		<?php endif; ?>
+		<?php endwhile; endif; ?>
 
-		<?php if( get_row_layout() == 'content_block' ) : ?>
+		<!-- End Vendor Gallery -->
 
-			<div class="container">
+		<!-- FAQs -->
+
+		<?php if( have_rows('faqs') ): ?>
+			<div class="container faqs__one">
 				<div class="row align-items-center">
-					<?php if( have_rows('contents') ) : while ( have_rows('contents') ) : the_row(); ?>
-						<div class="col content__block">
-							<?php if(get_sub_field('eyebrow')) : ?>
-								<span class="content__block-eyebrow"><?php the_sub_field('eyebrow'); ?></span>
-							<?php endif; ?>
-							<?php if(get_sub_field('heading')) : ?>
-								<h1 class="content__block-heading"><?php the_sub_field('heading'); ?></h1>
-							<?php endif; ?>
-							<div class="content__block-content"><?php the_sub_field('content'); ?></div>
-						</div>
-					<?php endwhile; endif; ?>
+					<div class="col-md-8 offset-md-2">
+						<?php while( have_rows('faqs') ): the_row(); ?>
+
+							<div class="faq__question">
+								<h2><?php the_sub_field('question'); ?></h2>
+							</div>
+							<div class="faq__answer">
+								<?php echo wpautop(get_sub_field('answer')); ?>
+							</div>
+
+						<?php endwhile; ?>
+					</div>
 				</div>
 			</div>
-
 		<?php endif; ?>
 
-		<?php endwhile; endif; ?>
+		<!-- End FAQs -->
 
 	</section>
 
@@ -81,7 +103,68 @@
 
 </main>
 
+<script>
+jQuery(document).ready(function($) {
+	$('.faq__question').click(function() {
+		$(this).toggleClass('faqs__clicked');
+		$(this).next().slideToggle();
+	});
+});
+</script>
+
 <style>
+.faqs__one h2 {
+  font-size: 1.5rem;
+	margin-bottom: 0;
+	transition: all .3s ease;
+}
+.faqs__one .faq__question {
+  border: 1px solid rgba(0,0,0,.2);
+  padding: 1.5rem;
+	margin-top: 1.5rem;
+	position: relative;
+	overflow: hidden;
+	cursor: pointer;
+	transition: all .3s ease;
+}
+.faqs__one .faq__question:nth-child(1) {
+	margin-top: 0;
+}
+.faqs__one .faq__question::after {
+  content: '+';
+  color: var(--primary);
+  position: absolute;
+  right: 2rem;
+  top: 50%;
+  transform: translateY(-55%);
+  font-size: 2rem;
+  font-weight: 600;
+	transition: all .3s ease;
+}
+.faqs__one .faq__question:hover, .faqs__clicked {
+	background: var(--primary);
+}
+.faqs__one .faq__question:hover > h2, .faqs__clicked h2 {
+	color: white;
+}
+.faqs__one .faq__question:hover::after {
+	color: white;
+}
+.faq__question.faqs__clicked::after {
+	color: white;
+	transform: translateY(-45%) rotate(135deg);
+}
+.faqs__one .faq__answer {
+	border: 1px solid rgba(0,0,0,.2);
+	border-top: none;
+	background: #f9f9f9;
+	padding: 1.5rem;
+	display: none;
+}
+.centered {
+	margin: 0 auto;
+  text-align: center;
+}
 section {
 	padding: 6.5rem 0;
 	background-size: cover;
@@ -117,16 +200,14 @@ main {
 	font-weight: 600;
 	display: block;
 	margin-bottom: 1rem;
+	width: 100%;
 }
 .content__block-heading {
-	font-weight: 600;
 	display: block;
 	margin-bottom: 1.5rem;
 	max-width: 75%;
 }
 .content__block-content p {
-	font-size: 1.2rem;
-	line-height: 1.75;
 	max-width: 85%;
 }
 </style>
