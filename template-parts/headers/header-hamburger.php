37fd5b2022219058c66cf5197cd9f8fdb0f50405
syntheticmachine <matt@ugduck.com>
2019-04-16 17:48:11 -0700

    dhj

--- template-parts/headers/header-hamburger.php
+++ template-parts/headers/header-hamburger.php
@@ -196,7 +196,8 @@ jQuery(document).ready(function($) {
 		$('body').toggleClass('noverflow');
 		$('.header__hamburger-menu').toggleClass('toggled');
 	});
-	$('.header__hamburger-menu ul .menu-item-has-children').click(function() {
+	$('.header__hamburger-menu ul .menu-item-has-children').click(function(e) {
+		e.preventDefault();
 		$(this).find('.sub-menu').slideToggle();
 		$(this).children('a').toggleClass('primary-color');
 	});
