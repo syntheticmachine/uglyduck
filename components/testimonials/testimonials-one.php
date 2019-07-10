7c406917d840528c6ae8d7efb64f91b303aab475
syntheticmachine <matt@ugduck.com>
2019-07-10 15:51:08 -0700

    adfd

--- components/testimonials/testimonials-one.php
+++ components/testimonials/testimonials-one.php
@@ -38,3 +38,127 @@
 
 	</div>
 </section>
+
+
+<script>
+	jQuery(document).ready(function($) {
+
+		$('.variable-width-slider').slick({
+			dots: false,
+		  infinite: true,
+		  speed: 500,
+		  slidesToShow: 4,
+		  centerMode: false,
+		  variableWidth: true,
+			autoplay: true,
+	  	autoplaySpeed: 4000,
+			prevArrow: '<button type="button" class="slick-prev"><i class="ion-chevron-left"></i></button>',
+			nextArrow: '<button type="button" class="slick-next"><i class="ion-chevron-right"></i></button>',
+			responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1,
+	        infinite: true,
+					variableWidth: false,
+	      }
+	    }]
+		});
+
+		$('.testimonial__section-one').slick({
+			dots: false,
+			infinite: true,
+			speed: 500,
+			slidesToShow: 2,
+			centerMode: false,
+			autoplay: true,
+			autoplaySpeed: 4000,
+			prevArrow: '<button type="button" class="slick-prev"><i class="ion-chevron-left"></i></button>',
+			nextArrow: '<button type="button" class="slick-next"><i class="ion-chevron-right"></i></button>',
+			responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+					variableWidth: false,
+				}
+			}]
+		});
+
+		$(".testimonial__text").each(function() {
+			if($(this).text().length > 195) {
+				$(this).addClass('readmore');
+				$(this).text(function(index, currentText) {
+					return currentText.substr(0, 195) + '...';
+				});
+			}
+		});
+
+		$('.faq__section-container').click(function() {
+			$(this).find('.faq__section-answer').slideToggle('fast');
+			$(this).toggleClass('toggled');
+		});
+	});
+</script>
+
+<style>
+.job-title {
+  color: #888;
+  font-weight: 500;
+  display: block;
+  position: absolute;
+  bottom: 2px;
+  font-size: 14px;
+}
+.testimonial__section {
+	background: #F4F8FB;
+	padding: 2rem 0 !important;
+}
+.testimonial__container {
+	padding: 1rem;
+}
+.testimonial__container i {
+	display: block;
+	font-size: 1.35rem;
+	color: var(--primary);
+	margin-bottom: 1.5rem;
+	margin-top: .5rem;
+}
+.testimonial__container h4 {
+	font-size: 1.1rem;
+}
+.testimonial__container-inner {
+	border: 1px solid rgba(0,0,0,.1);
+	background: white;
+	padding: 1.5rem;
+	padding-top: 2rem;
+	padding-bottom: 2rem;
+}
+.testimonial__container-image {
+	width: 40px;
+	height: 40px;
+	border-radius: 100pc;
+	position: relative;
+	overflow: hidden;
+	border: 2px solid rgba(0,0,0,.1);
+}
+.testimonial__text {
+	font-size: 1.05rem;
+	margin-bottom: 1.5rem;
+	min-height: 125px;
+}
+.readmore::after {
+	content: 'Read Full Review';
+	font-weight: 500;
+	color: var(--primary);
+}
+.testimonial__section .slick-next {
+	right: -85px;
+}
+.testimonial__section .slick-prev {
+	left: -85px;
+}
+</style>
